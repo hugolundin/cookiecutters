bc7399bcf1e3b7ea4bb3ff765c37aa167c715a08
Hugo Lundin <hugo@lundin.dev>
2020-10-14 22:25:14 +0200

    Add trace

--- verilog/{{cookiecutter.project_slug}}/test/main.cpp
+++ verilog/{{cookiecutter.project_slug}}/test/main.cpp
@@ -1,4 +1,5 @@
 #include <verilated.h>
+#include <verilated_vcd_c.h>
 #include <Vtop.h>
 #include <iostream>
 
@@ -7,7 +8,12 @@ vluint64_t main_time = 0;
 int main()
 {
     Vtop top;
+    VerilatedVcdC tf;
+
     Verilated::debug(0);
+    Verilated::traceEverOn(true);
+    top.trace(&tf, 99);
+    tf.open("dump.vcd");
 
     top.i1 = 1;
     top.i2 = 2;
@@ -17,9 +23,12 @@ int main()
         main_time += 1;
         top.clk = !top.clk;
         top.eval();
+        tf.dump(main_time);
 
         VL_PRINTF("[%" VL_PRI64 "d] clk=%x result=%x\n", main_time, top.clk, top.result);
     }
 
     top.final();
+    tf.close();
 }
+
